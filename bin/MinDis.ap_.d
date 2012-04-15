89326f2baa0e87e2ff82965e51a5cedd932ea830
Bryce <tonyhawk2100@gmail.com>
2012-04-15 09:27:45 -0700

    save new files

---
+++ bin/MinDis.ap_.d
@@ -0,0 +1,12 @@
+/Users/tonyhawk2100/Programs/MinDis/bin/MinDis.ap_ \
+ : /Users/tonyhawk2100/Programs/MinDis/res/drawable-hdpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/res/drawable-ldpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/res/drawable-mdpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/res/layout/main.xml \
+/Users/tonyhawk2100/Programs/MinDis/res/menu/app_menu.xml \
+/Users/tonyhawk2100/Programs/MinDis/res/values/arrays.xml \
+/Users/tonyhawk2100/Programs/MinDis/res/values/strings.xml \
+/Users/tonyhawk2100/Programs/MinDis/bin/res/drawable-hdpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/bin/res/drawable-ldpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/bin/res/drawable-mdpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/AndroidManifest.xml \
---
+++ bin/classes.dex.d
@@ -0,0 +1,17 @@
+/Users/tonyhawk2100/Programs/MinDis/bin/classes.dex : \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/calcDate.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/Eula$1.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/Eula$2.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/Eula.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/MinDis$1.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/MinDis.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/prevDate.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$array.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$attr.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$drawable.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$id.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$layout.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$menu.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R$string.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/R.class \
+/Users/tonyhawk2100/Programs/MinDis/bin/classes/com/MinDis/android/RMD.class \
