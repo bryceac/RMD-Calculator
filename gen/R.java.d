3eec8cfe63c15f6a6ca9bae2530c2ace74b75841
Bryce <tonyhawk2100@gmail.com>
2012-04-15 09:15:34 -0700

    fix source code

---
+++ gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/tonyhawk2100/Programs/MinDis/gen/com/MinDis/android/R.java \
+ : /Users/tonyhawk2100/Programs/MinDis/res/drawable-hdpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/res/drawable-ldpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/res/drawable-mdpi/icon.png \
+/Users/tonyhawk2100/Programs/MinDis/res/layout/main.xml \
+/Users/tonyhawk2100/Programs/MinDis/res/menu/app_menu.xml \
+/Users/tonyhawk2100/Programs/MinDis/res/values/arrays.xml \
+/Users/tonyhawk2100/Programs/MinDis/res/values/strings.xml \
+/Users/tonyhawk2100/Programs/MinDis/AndroidManifest.xml \
